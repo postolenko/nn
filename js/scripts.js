5951f59088e80310f2384f2b57a2b9de62ce57a6
postolenko <denis.postolenko@gmail.com>
2017-12-09 04:40:51 +0200

    NN

--- js/scripts.js
+++ js/scripts.js
@@ -28,12 +28,61 @@ $(document).ready(function () {
 
     // ---------------------------
 
-
-
     $( "#price-slider" ).slider({
         range: true,
         values: [ 17, 67 ]
     });
+
+    // ---------------------------
+
+    var parentBlock;
+    var slidingBlock;
+    var slidingBtn;
+
+    $(".sliding-block_wrapp").each(function() {
+
+        slidingBlock = $(this).find(".sliding-block");
+
+        slidingBtn = $(this).find(".sliding-btn");
+
+        if( $(this).hasClass("active")) {           
+
+            slidingBlock.slideDown(30);
+
+            slidingBtn.addClass("active");
+
+        } else {
+
+            slidingBlock.slideUp(30);
+
+            slidingBtn.removeClass("active");
+
+        }
+
+    });
+
+    $(".sliding-btn").click(function() {
+
+        parentBlock = $(this).closest(".sliding-block_wrapp");
+
+        slidingBlock = parentBlock.find(".sliding-block");
+
+        if(slidingBlock.is(":visible")) {
+
+            slidingBlock.slideUp(600);
+
+            $(this).removeClass("active");
+
+        } else {
+
+            slidingBlock.slideDown(600);
+
+            $(this).addClass("active");
+
+        }
+
+    });
+
 });
 
 //Active Class in menu
