c22e70756ccf977b131bf7c1821ab4dcf302c028
postolenko <denis.postolenko@gmail.com>
2017-12-19 21:51:41 +0200

    Actionteaser

--- js/map.js
+++ js/map.js
@@ -1,26 +1,16 @@
 ymaps.ready(init);
-var myMap,
-    myPlacemark;
 
-function init(){   
-    myMap = new ymaps.Map("map_route", {
+function init(){
+
+    var myMap = new ymaps.Map("map_route", {
         center: [55.733677, 37.587874],
         zoom: 16
     });
 
-    myPlacemark = new ymaps.Placemark([55.733670, 37.587874], { 
+    var myPlacemark = new ymaps.Placemark([55.733670, 37.587874], { 
         hintContent: 'NN', 
         balloonContent: 'NN'
-    }
-
-    // , {
-    //     iconLayout: 'default#image',
-    //     iconImageHref: 'img/map_marker.png',
-    //     iconImageSize: [102, 85],
-    //     iconImageOffset: [-36, -76]
-    // }
-
-    );
+    });
 
     myMap.behaviors
     .disable(['scrollZoom', 'rightMouseButtonMagnifier']);
--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,13 @@
 
 $(document).ready(function () {
 
-    $('.owl-carousel.carousel-main-slider').owlCarousel({
+    var mainCarousel = $('.owl-carousel.carousel-main-slider').owlCarousel({
         loop:false,
         mouseDrag: false,
         touchDrag: true,
         dots: true,
         nav: true,
+        onInitialized: activeThumb,
         responsiveClass:true,
         responsive:{
             0:{
@@ -26,156 +27,388 @@ $(document).ready(function () {
         }
     });
 
+    mainCarousel.on('changed.owl.carousel', function(event) {
+
+        var sliderNameAttr = $(this).attr("data-slider");
+
+        if( $("[data-slider-nav = '" + sliderNameAttr + "' ]").length > 0 ) {
+
+            var hashAttr = $(this).find(".owl-item:eq("+ event.item.index +") .carousel-item").attr("data-hash");
+
+            var miniaturesNav = $("[data-slider-nav = '"+ sliderNameAttr +"']");
+
+            miniaturesNav.find(".slide_nav").each(function() {
+
+                if( $(this).attr("href") == "#"+ hashAttr ) {
+
+                    $(this).addClass("active");
+
+                } else {
+
+                    $(this).removeClass("active");
+
+                }
+
+            });            
+
+        }
+
+    });
+
+    function activeThumb(event) {
+
+        var sliderName = $(event.target);
+
+        var sliderNameAttr = sliderName.attr("data-slider");
+
+        if( $("[data-slider-nav = '" + sliderNameAttr + "' ]").length > 0 ) {
+
+            var hashAttr = sliderName.find(".owl-item.active .carousel-item").attr("data-hash");
+
+            var miniaturesNav = $("[data-slider-nav = '"+ sliderNameAttr +"']");
+
+            miniaturesNav.find(".slide_nav").each(function() {
+
+            if( $(this).attr("href") == "#"+ hashAttr ) {
+
+                    $(this).addClass("active");
+
+                } else {
+
+                    $(this).removeClass("active");
+
+                }
+
+            });
+
+        }
+
+    }
+
     // ------------------------------------------
 
-    // var sliderName;
-    // var sliderNameAttr;
-    // var slideDesc;
-    // var slideDescTxt;
-    // var item;
-    // var hashAttr;
-    // var miniaturesNav;
+    var sliderName;
+    var sliderNameAttr;
+    var slideDesc;
+    var slideDescTxt;
+    var item;
+    var hashAttr;
+    var miniaturesNav;
 
-    // var gkSlider =  $('.gk-slider').owlCarousel({
-    //     loop: true,
-    //     mouseDrag: false,
-    //     touchDrag: true,
-    //     dots: true,
-    //     nav: true,
-    //     onInitialized: showItem,
-    //     responsiveClass: true,
-    //     responsive:{
-    //         0:{
-    //             items:1
-    //         },
-    //         600:{
-    //             items:1
+    var gkSlider =  $('.gk-slider').owlCarousel({
+        loop: true,
+        mouseDrag: false,
+        touchDrag: true,
+        dots: true,
+        nav: true,
+        onInitialized: showItem,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
 
-    //         },
-    //         992:{
-    //             items:1
-    //         },
-    //         1200:{
-    //             items:1
-    //         }
-    //     }
-    // });
+            },
+            992:{
+                items:1
+            },
+            1200:{
+                items:1
+            }
+        }
+    });
 
-    // gkSlider.on('changed.owl.carousel', function(event) {
+    gkSlider.on('changed.owl.carousel', function(event) {
 
-    //     item = event.item.index;
+        item = event.item.index;
 
-    //     hashAttr = $(this).find(".owl-item:eq("+ item +") .carousel-item").attr("data-hash");
+        hashAttr = $(this).find(".owl-item:eq("+ item +") .carousel-item").attr("data-hash");
 
-    //     miniaturesNav = $("[data-slider-miniatures = '"+ sliderNameAttr +"']");
+        miniaturesNav = $("[data-slider-miniatures = '"+ sliderNameAttr +"']");
 
-    //     miniaturesNav.find(".nav-thumb").each(function() {
+        miniaturesNav.find(".nav-thumb").each(function() {
 
-    //         if( $(this).attr("href") == "#"+ hashAttr ) {
+            if( $(this).attr("href") == "#"+ hashAttr ) {
 
-    //             $(this).addClass("active");
+                $(this).addClass("active");
 
-    //         } else {
+            } else {
 
-    //             $(this).removeClass("active");
+                $(this).removeClass("active");
 
-    //         }
+            }
 
-    //     });
+        });
 
-    //     slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
+        slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
 
-    //     slideDescTxt = $(this).find(".owl-item:eq("+ item +") .carousel-item").attr("data-descript");
+        slideDescTxt = $(this).find(".owl-item:eq("+ item +") .carousel-item").attr("data-descript");
 
-    //     slideDesc.text(slideDescTxt);
+        slideDesc.text(slideDescTxt);
 
-    // });
+    });
 
-    // function showItem(event) {
+    function showItem(event) {
 
-    //     sliderName = $(event.target);
+        sliderName = $(event.target);
 
-    //     sliderNameAttr = sliderName.attr("data-slider");
+        sliderNameAttr = sliderName.attr("data-slider");
 
-    //     hashAttr = sliderName.find(".owl-item.active .carousel-item").attr("data-hash");
+        hashAttr = sliderName.find(".owl-item.active .carousel-item").attr("data-hash");
 
-    //     miniaturesNav = $("[data-slider-miniatures = '"+ sliderNameAttr +"']");
+        miniaturesNav = $("[data-slider-miniatures = '"+ sliderNameAttr +"']");
 
-    //     miniaturesNav.find(".nav-thumb").each(function() {
+        miniaturesNav.find(".nav-thumb").each(function() {
 
-    //         if( $(this).attr("href") == "#"+ hashAttr ) {
+            if( $(this).attr("href") == "#"+ hashAttr ) {
 
-    //             $(this).addClass("active");
+                $(this).addClass("active");
 
-    //         } else {
+            } else {
 
-    //             $(this).removeClass("active");
+                $(this).removeClass("active");
 
-    //         }
+            }
 
-    //     });
+        });
 
-    //     slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
+        slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
 
-    //     slideDescTxt = $(event.target).find(".owl-item.active .carousel-item").attr("data-descript");
+        slideDescTxt = $(event.target).find(".owl-item.active .carousel-item").attr("data-descript");
 
-    //     slideDesc.text(slideDescTxt);
+        slideDesc.text(slideDescTxt);
 
-    // }
+    }
 
-    // var photoGallery =  $('.photogallery-slider').owlCarousel({
-    //     loop:false,
-    //     mouseDrag: false,
-    //     touchDrag: true,
-    //     dots: true,
-    //     nav: true,
-    //     onInitialized: showItem2,
-    //     responsiveClass: true,
-    //     responsive:{
-    //         0:{
-    //             items:1
-    //         },
-    //         600:{
-    //             items:1
+    var photoGallery =  $('.photogallery-slider').owlCarousel({
+        loop:false,
+        mouseDrag: false,
+        touchDrag: true,
+        dots: true,
+        nav: true,
+        onInitialized: showItem2,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
 
-    //         },
-    //         992:{
-    //             items:1
-    //         },
-    //         1200:{
-    //             items:1
-    //         }
-    //     }
-    // });
+            },
+            992:{
+                items:1
+            },
+            1200:{
+                items:1
+            }
+        }
+    });
 
-    // photoGallery.on('changed.owl.carousel', function(event) {
+    photoGallery.on('changed.owl.carousel', function(event) {
 
-    //     item = event.item.index;
+        item = event.item.index;
 
-    //     sliderNameAttr = $(this).attr("data-slider");
+        sliderNameAttr = $(this).attr("data-slider");
 
-    //     slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
+        slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
 
-    //     slideDescTxt = $(this).find(".owl-item:eq("+ item +") .carousel-item").attr("data-descript");
+        slideDescTxt = $(this).find(".owl-item:eq("+ item +") .carousel-item").attr("data-descript");
 
-    //     slideDesc.text(slideDescTxt);
+        slideDesc.text(slideDescTxt);
 
-    // });
+    });
 
-    // function showItem2(event) {
+    function showItem2(event) {
 
-    //     sliderName = $(event.target);
+        sliderName = $(event.target);
 
-    //     sliderNameAttr = sliderName.attr("data-slider");
+        sliderNameAttr = sliderName.attr("data-slider");
 
-    //     slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
+        slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
 
-    //     slideDescTxt = $(event.target).find(".owl-item.active .carousel-item").attr("data-descript");
+        slideDescTxt = $(event.target).find(".owl-item.active .carousel-item").attr("data-descript");
 
-    //     slideDesc.text(slideDescTxt);
+        slideDesc.text(slideDescTxt);
 
-    // }
+    }
 
+    // ---------------------------
+
+    var videoSlider =  $('.video-slider').owlCarousel({
+        loop: true,
+        mouseDrag: false,
+        touchDrag: true,
+        dots: true,
+        nav: true,
+        onInitialized: showItem3,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+
+            },
+            992:{
+                items:1
+            },
+            1200:{
+                items:1
+            }
+        }
+    });
+
+    videoSlider.on('changed.owl.carousel', function(event) {
+
+        item = event.item.index;
+        
+        sliderNameAttr = $(this).attr("data-slider");
+
+        hashAttr = $(this).find(".owl-item:eq("+ item +") .carousel-item").attr("data-hash");
+
+        miniaturesNav = $("[data-slider-miniatures = '"+ sliderNameAttr +"']");
+
+        miniaturesNav.find(".nav-thumb").each(function() {
+
+            if( $(this).attr("href") == "#"+ hashAttr ) {
+
+                $(this).addClass("active");
+
+            } else {
+
+                $(this).removeClass("active");
+
+            }
+
+        });
+
+        slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
+
+        slideDescTxt = $(this).find(".owl-item:eq("+ item +") .carousel-item").attr("data-descript");
+
+        slideDesc.text(slideDescTxt);
+
+    });
+
+    function showItem3(event) {
+
+        sliderName = $(event.target);
+
+        sliderNameAttr = sliderName.attr("data-slider");
+
+        hashAttr = sliderName.find(".owl-item.active .carousel-item").attr("data-hash");
+
+        miniaturesNav = $("[data-slider-miniatures = '"+ sliderNameAttr +"']");
+
+        miniaturesNav.find(".nav-thumb").each(function() {
+
+            if( $(this).attr("href") == "#"+ hashAttr ) {
+
+                $(this).addClass("active");
+
+            } else {
+
+                $(this).removeClass("active");
+
+            }
+
+        });
+
+        slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
+
+        slideDescTxt = $(event.target).find(".owl-item.active .carousel-item").attr("data-descript");
+
+        slideDesc.text(slideDescTxt);
+
+    }
+
+    // ---------------------------
+
+    var objectSecondary =  $('.object-secondary_slider').owlCarousel({
+        loop:false,
+        mouseDrag: false,
+        touchDrag: true,
+        dots: true,
+        nav: true,
+        onInitialized: showItem4,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+
+            },
+            992:{
+                items:1
+            },
+            1200:{
+                items:1
+            }
+        }
+    });
+
+    objectSecondary.on('changed.owl.carousel', function(event) {
+
+        var item = event.item.index;
+
+        var sliderNameAttr = $(this).attr("data-slider");
+
+        var slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
+
+        var slideDescTxt = $(this).find(".owl-item:eq("+ item +") .carousel-item").attr("data-descript");
+
+        slideDesc.text(slideDescTxt);
+
+    });
+
+    function showItem4(event) {
+
+        var sliderName = $(event.target);
+
+        var sliderNameAttr = sliderName.attr("data-slider");
+
+        var slideDesc  = $(".slide-descript-row[data-slider-desc = '"+ sliderNameAttr + "']");
+
+        var slideDescTxt = $(event.target).find(".owl-item.active .carousel-item").attr("data-descript");
+
+        slideDesc.text(slideDescTxt);
+
+    }
+
+    if( $(".gk-slider-2").length > 0 ) {
+
+        $(".gk-slider-2").owlCarousel({
+            loop: true,
+            mouseDrag: false,
+            touchDrag: true,
+            dots: false,
+            nav: false,
+            onInitialized: showItem,
+            responsiveClass: true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+
+                },
+                992:{
+                    items:1
+                },
+                1200:{
+                    items:1
+                }
+            }
+        });
+
+    }
 
     // ---------------------------
 
